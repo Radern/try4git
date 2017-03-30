350765426e224032f31d772181ebf3989e8c6ab4
Radern <spam2@radern.com>
2017-03-30 14:45:42 +0200

    v2.a

--- src/git4try/HelloWorld.java
+++ src/git4try/HelloWorld.java
@@ -1,10 +1,10 @@
 package git4try;
 
-public class HeloWorld {
+public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("aaabbb");
+		System.out.println("HelloWorld");
 	}
 
 }
